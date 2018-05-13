3f699dc652c36e220379d4b77cf1316f0a761615
Mark Kohler <mark.kohler@gmail.com>
2018-05-12 20:46:21 -0700

    Ran elm-format. Add password length restriction.

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -1,14 +1,17 @@
+module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
+import String exposing (length)
 
 
 main =
-  Html.beginnerProgram
-    { model = model
-    , view = view
-    , update = update
-    }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
 
 
 
@@ -16,15 +19,15 @@ main =
 
 
 type alias Model =
-  { name : String
-  , password : String
-  , passwordAgain : String
-  }
+    { name : String
+    , password : String
+    , passwordAgain : String
+    }
 
 
 model : Model
 model =
-  Model "" "" ""
+    Model "" "" ""
 
 
 
@@ -39,15 +42,15 @@ type Msg
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    Name name ->
-      { model | name = name }
+    case msg of
+        Name name ->
+            { model | name = name }
 
-    Password password ->
-      { model | password = password }
+        Password password ->
+            { model | password = password }
 
-    PasswordAgain password ->
-      { model | passwordAgain = password }
+        PasswordAgain password ->
+            { model | passwordAgain = password }
 
 
 
@@ -56,21 +59,32 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ input [ type_ "text", placeholder "Name", onInput Name ] []
-    , input [ type_ "password", placeholder "Password", onInput Password ] []
-    , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
-    , viewValidation model
-    ]
+    div []
+        [ input [ type_ "text", placeholder "Name", onInput Name ] []
+        , input [ type_ "password", placeholder "Password", onInput Password ] []
+        , input [ type_ "password", placeholder "Re-enter Password", onInput PasswordAgain ] []
+        , viewValidation model
+        ]
 
 
 viewValidation : Model -> Html msg
 viewValidation model =
-  let
-    (color, message) =
-      if model.password == model.passwordAgain then
-        ("green", "OK")
-      else
-        ("red", "Passwords do not match!")
-  in
-    div [ style [("color", color)] ] [ text message ]
+    let
+        ( color, message ) =
+            if not (isGoodPassword model.password) then
+                ( "red", "Password needs to be longer than 8 characters." )
+            else if model.password == model.passwordAgain then
+                ( "green", "OK" )
+            else
+                ( "red", "Passwords do not match!" )
+    in
+        div [ style [ ( "color", color ) ] ] [ text message ]
+
+
+isGoodPassword : String -> Bool
+isGoodPassword password =
+    length password > 8
+
+
+
+-- isGoodPassword password =
