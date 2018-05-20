4de176e942f233fe25adad92f899371b2252fb03
Mark Kohler <mark.kohler@gmail.com>
2018-05-19 23:27:43 -0700

    04-random. Draw my own dice with svg.

--- examples/04-random.elm
+++ examples/04-random.elm
@@ -5,6 +5,8 @@ import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 import Random
+import Svg
+import Svg.Attributes
 
 
 main =
@@ -67,15 +69,148 @@ view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Roll ] [ text "Roll" ]
-        , img
-            [ src (getDieFace model.dieFace1) ]
-            []
-        , img
-            [ src (getDieFace model.dieFace2) ]
-            []
+        , drawDie model.dieFace1
+        , drawDie model.dieFace2
         ]
 
 
-getDieFace : Int -> String
-getDieFace val =
-    "Dice-" ++ toString (val) ++ ".svg"
+drawDie val =
+    Svg.svg
+        svgDieAttributes
+        ([ svgDieRect
+         ]
+            ++ svgDie val
+        )
+
+
+svgDieAttributes =
+    [ Svg.Attributes.width "500"
+    , Svg.Attributes.height "500"
+    , Svg.Attributes.viewBox "0 0 120 120"
+    ]
+
+
+svgDieRect =
+    Svg.rect
+        [ Svg.Attributes.x "10"
+        , Svg.Attributes.y "10"
+        , Svg.Attributes.width "100"
+        , Svg.Attributes.height "100"
+        , Svg.Attributes.rx "15"
+        , Svg.Attributes.ry "15"
+        , Svg.Attributes.stroke "#000000"
+        , Svg.Attributes.strokeWidth "3"
+        , Svg.Attributes.fill "none"
+        ]
+        []
+
+
+svgDotCenter =
+    Svg.circle
+        [ Svg.Attributes.cx "60"
+        , Svg.Attributes.cy "60"
+        , Svg.Attributes.r "10"
+        ]
+        []
+
+
+svgDotUpperLeft =
+    Svg.circle
+        [ Svg.Attributes.cx "30"
+        , Svg.Attributes.cy "30"
+        , Svg.Attributes.r "10"
+        ]
+        []
+
+
+svgDotUpperRight =
+    Svg.circle
+        [ Svg.Attributes.cx "90"
+        , Svg.Attributes.cy "30"
+        , Svg.Attributes.r "10"
+        ]
+        []
+
+
+svgDotLowerRight =
+    Svg.circle
+        [ Svg.Attributes.cx "90"
+        , Svg.Attributes.cy "90"
+        , Svg.Attributes.r "10"
+        ]
+        []
+
+
+svgDotLowerLeft =
+    Svg.circle
+        [ Svg.Attributes.cx "30"
+        , Svg.Attributes.cy "90"
+        , Svg.Attributes.r "10"
+        ]
+        []
+
+
+svgDotCenterLeft =
+    Svg.circle
+        [ Svg.Attributes.cx "30"
+        , Svg.Attributes.cy "60"
+        , Svg.Attributes.r "10"
+        ]
+        []
+
+
+svgDotCenterRight =
+    Svg.circle
+        [ Svg.Attributes.cx "90"
+        , Svg.Attributes.cy "60"
+        , Svg.Attributes.r "10"
+        ]
+        []
+
+
+svgDie value =
+    case value of
+        1 ->
+            svgDieOne
+
+        2 ->
+            svgDieTwo
+
+        3 ->
+            svgDieThree
+
+        4 ->
+            svgDieFour
+
+        5 ->
+            svgDieFive
+
+        6 ->
+            svgDieSix
+
+        _ ->
+            svgDieTwo
+
+
+svgDieOne =
+    [ svgDotCenter ]
+
+
+svgDieTwo =
+    [ svgDotUpperLeft, svgDotLowerRight ]
+
+
+svgDieThree =
+    [ svgDotUpperLeft, svgDotCenter, svgDotLowerRight ]
+
+
+svgDieFour =
+    [ svgDotUpperLeft, svgDotUpperRight, svgDotLowerLeft, svgDotLowerRight ]
+
+
+svgDieFive =
+    svgDieFour ++ svgDieOne
+
+
+svgDieSix =
+    svgDieFour ++ [ svgDotCenterLeft, svgDotCenterRight ]
