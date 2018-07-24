e5f4bdff6a99ed51122b6c5258a411a5312e0707
Mark Kohler <mark.kohler@proteinsimple.com>
2018-07-23 20:50:51 -0700

    06-clock. Pause clock (but the button doesn't change.)

--- examples/06-clock.elm
+++ examples/06-clock.elm
@@ -1,29 +1,35 @@
-import Html exposing (Html)
+module Main exposing (..)
+
+import Html
+import Html exposing (Html, button, div)
+import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 import Time exposing (Time, second)
 
 
-
 main =
-  Html.program
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
 -- MODEL
 
 
-type alias Model = Time
+type alias Model =
+    { time : Time
+    , paused : Bool
+    }
 
 
-init : (Model, Cmd Msg)
+init : ( Model, Cmd Msg )
 init =
-  (0, Cmd.none)
+    ( { time = 0.0, paused = False }, Cmd.none )
 
 
 
@@ -31,14 +37,21 @@ init =
 
 
 type Msg
-  = Tick Time
+    = Tick Time
+    | TogglePause
 
 
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Tick newTime ->
-      (newTime, Cmd.none)
+    case msg of
+        Tick newTime ->
+            if model.paused then
+                ( { model | time = newTime }, Cmd.none )
+            else
+                ( model, Cmd.none )
+
+        TogglePause ->
+            ( { model | paused = not model.paused }, Cmd.none )
 
 
 
@@ -47,7 +60,7 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second Tick
+    Time.every second Tick
 
 
 
@@ -56,17 +69,20 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-  let
-    angle =
-      turns (Time.inMinutes model)
+    let
+        angle =
+            turns (Time.inMinutes model.time)
 
-    handX =
-      toString (50 + 40 * cos angle)
+        handX =
+            toString (50 + 40 * cos angle)
 
-    handY =
-      toString (50 + 40 * sin angle)
-  in
-    svg [ viewBox "0 0 100 100", width "300px" ]
-      [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
-      , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
-      ]
+        handY =
+            toString (50 + 40 * sin angle)
+    in
+        div []
+            [ svg [ viewBox "0 0 100 100", width "300px" ]
+                [ circle [ cx "50", cy "50", r "45", fill "#0B79CE" ] []
+                , line [ x1 "50", y1 "50", x2 handX, y2 handY, stroke "#023963" ] []
+                ]
+            , button [ onClick TogglePause ] [ Html.text "Pause" ]
+            ]
